146b8892dad138b82dac587e06ee92f9ee0f3207
darkleaderken@gmail.com <darkleaderken@gmail.com>
2020-12-01 20:27:03 +0700

    UPDATE PDF

--- src/app/component/customer/pdf-viewer-customer/pdf-viewer-customer.component.ts
+++ src/app/component/customer/pdf-viewer-customer/pdf-viewer-customer.component.ts
@@ -1,67 +1,50 @@
-import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, HostListener, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { defaultBase64} from '../default';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { DeviceDetectorService } from 'ngx-device-detector';
 import { Platform } from '@angular/cdk/platform';
-import { style } from '@angular/animations';
-import {MatIconRegistry} from '@angular/material/icon';
-import {DomSanitizer} from '@angular/platform-browser';
-
-
+import { MatIconRegistry} from '@angular/material/icon';
+import { DomSanitizer} from '@angular/platform-browser';
 @Component({
   selector: 'app-pdf-viewer-customer',
   templateUrl: './pdf-viewer-customer.component.html',
   styleUrls: ['./pdf-viewer-customer.component.scss']
 })
-export class PdfViewerCustomerComponent implements OnInit {
+export class PdfViewerCustomerComponent implements OnInit, OnChanges {
+  @Output('onLoadComplete') loadComplete = new EventEmitter();
   @Output('onReadComplete') readComplete = new EventEmitter();
   @Output('onLoadPdfError') loadPdfError = new EventEmitter();
   @Input() sourcePdf: string;
-  pdfPage:number = 1;
-  pdf:any;
-  src = defaultBase64;
-  howMuchPage:number = 0;
+  pdfPage: number = 1;
+  pdf: any;
+  src = defaultBase64[0];
+  howMuchPage: number = 0;
   toolTip = false;
-  backGroundColorPdf:string ="white";
+  backGroundColorPdf: string = "white";
+  isHiddenSpinner: boolean;
   zoomLevels = ['auto', 'page-actual', 'page-fit', 'page-width',
-    0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0,
-    1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0,
-    2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0,
-    3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0];
-  _zoomSetting: number | string | undefined = 'page-width';
-  _oldZoomSetting: number | string | undefined = 'page-width'
-  eventFullScreen:boolean = true;
+    0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0];
+  _oldZoomSetting: number | string | undefined = 'page-width';
+  eventFullScreen: boolean = true;
+  flagDownloadPdf: boolean = true;
   //Platform
   isMobile: boolean = false;
   isSafari: boolean = false;
   isAndroid: boolean = false;
-  sizePlatForm:string = "80vh"
-  sizeOldPlatForm:string = "80vh"
-  get zoomSetting() {
-    return String(this._zoomSetting);
-  }
-  set zoomSetting(zoom: string) {
-    if (isNaN(Number(zoom))) {
-      this._zoomSetting = zoom;
-    } else {
-      this._zoomSetting = zoom + '%';
-    }
-  }
-
-  /**
-     * Property custom
-     * @param showToolbar
-     * @param sidebarToggle
-  */
-  showToolbar:boolean = true;
-  sidebarToggle:string ="none"
-  hozFullScreen:boolean = false;
+  sizePlatForm: string = "72vh";
+  sizeOldPlatForm: string = "72vh";
+  viewMode: string;
+  showToolbar: boolean = true;
+  sidebarToggle: string = "none";
+  hozFullScreen: boolean = false;
+  flagFullScreen: boolean = false;
   constructor(
     private http: HttpClient,
     private platform: Platform,
     private deviceService: DeviceDetectorService,
     private iconRegistry: MatIconRegistry,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private cdr: ChangeDetectorRef,
     ) {
         this.iconRegistry.addSvgIcon(
           'exit-full-screen',
@@ -69,72 +52,98 @@ export class PdfViewerCustomerComponent implements OnInit {
      }
 
   ngOnInit(): void {
+    // this.isHiddenSpinner = true;
     this.isSafari = this.platform.SAFARI;
     this.isAndroid = this.platform.ANDROID;
-    this.isMobile = this.deviceService.isMobile() ||  this.platform.IOS || this.isAndroid;
+    this.isMobile = this.deviceService.isMobile() || this.platform.IOS || this.isAndroid;
     if (this.isMobile){
-
-      this.zoomSetting = "page-fit";
-    }
-    else{
-      this.zoomSetting = "auto";
+      this.viewMode = "single";
+    } else{
+      this.viewMode = "single";
     }
     this._oldZoomSetting = 0;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void{
     this.getPdfFormServer();
   }
-  /**
-     * Setting config inital for pdf
-  */
   settingPDF(){
     return {
-      backGroundColor:this.backGroundColorPdf,
-      isPinchOnMobile:false,
-      handTool:false,
-      fullscreen:true,
-      scrollbar:true,
-      zoom:true,
-      showBorders:true,
-      showBookmarkButton:false,
-      showPagingButtons:true,
-      showPrintButton:true,
-      showPropertiesButton:false,
-      showSidebarButton:false,
-      sidebarVisible:false,
-      showSpreadButton:true,
-
+      backGroundColor: this.backGroundColorPdf,
+      isPinchOnMobile: true,
+      handTool: true,
+      fullscreen: false,
+      scrollbar: true,
+      zoom: true,
+      showBorders: true,
+      showBookmarkButton: false,
+      showPagingButtons: true,
+      showPrintButton: false,
+      showPropertiesButton: true,
+      showSidebarButton: false,
+      sidebarVisible: false,
+      showSpreadButton: true,
+      showOpenFileButton: false,
+      mobileFriendlyZoom: true
     };
    }
 
-  /**
-     * When page loaded
-  */
+   onPdfLoad(event){
+     //doing something
+   }
+   romanToInt(str1) {
+      let  num = this.charToInt( str1.charAt(0) );
+      let  pre;
+      let curr;
+      for (let i = 1; i < str1.length; i++){
+        curr = this.charToInt( str1.charAt(i) );
+        pre = this.charToInt( str1.charAt(i - 1) );
+        if(curr <= pre){
+          num += curr;
+        } else {
+          num = num - pre * 2 + curr;
+        }
+      }
+      return num;
+  }
+  charToInt(c){
+    switch (c){
+      case 'i': return 1;
+      case 'v': return 5;
+      case 'x': return 10;
+      case 'l': return 50;
+      case 'c': return 100;
+      case 'd': return 500;
+      case 'm': return 1000;
+      default: return -1;
+    }
+  }
   pagesLoaded(event){
+    if (this.isMobile){
+      const h = event.source.viewer.clientHeight;
+      this.sizePlatForm = (h + 50) + 'px';
+      this.sizeOldPlatForm = this.sizePlatForm ;
+      this.cdr.detectChanges();
+    }
     this.pdf = event;
-    this.howMuchPage = event.pagesCount
+    this.howMuchPage = event.pagesCount;
     const viewContainer = document.getElementById("viewerContainer");
-    viewContainer.style.border ="2px solid #DCDCDC"
-    // const sidebarToggle = document.getElementById("sidebarToggle")
-    // sidebarToggle.style.display=this.sidebarToggle
+    viewContainer.style.border = "2px solid #DCDCDC";
+    this.onReadComplete();
+    this.loadComplete.emit(true);
   }
 
-
-   /**
-     * when page change
-  */
    onPageChange(event){
-     this.onReadedComple();
+     this.onReadComplete();
    }
 
-   /**
-     * When page change and rendered
-  */
    onPageRendered(event){
-
+     //doing something
    }
-
    onPdfLoadingFailed(event){
      if (event){
-      this.loadPdfError.emit(true)
+      this.loadPdfError.emit(true);
+      this.src = defaultBase64[1];
      }
    }
 
@@ -145,8 +154,7 @@ export class PdfViewerCustomerComponent implements OnInit {
      if (amount === 1){
       const element = document.getElementById("next");
       element.dispatchEvent(new Event("click"));
-     }
-     else{
+     } else{
        if (amount === -1){
         const element = document.getElementById("previous");
         element.dispatchEvent(new Event("click"));
@@ -157,33 +165,34 @@ export class PdfViewerCustomerComponent implements OnInit {
    /**
     * Download pdf
     */
-  getPdfFormServer() {
-    this.http.get<any>(this.sourcePdf, {
-          responseType: 'blob' as 'json',
-          headers: new HttpHeaders({
-            'Content-Type':'application/json',
-            'Access-Control-Allow-Origin' :"*",
-            'Access-Control-Allow-Headers': "Content-Type",
-            'Access-Control-Allow-Methods': "GET,POST",
-            'Accept': 'text/plain, */*',
-            'Cache-Control': 'no-cache',
-        })
-      }).subscribe((response) => {
+
+   getPdfFormServer() {
+    this.pdfPage = 1;
+    this.isHiddenSpinner = false;
+    fetch(this.sourcePdf)
+      .then((response) => {
+        return response.blob();
+      })
+      .then( (myBlob) => {
         const data = new Promise((resolve, reject) => {
           const reader = new FileReader();
           reader.onerror = reject;
           reader.onload = () => {
-            const base64 = reader.result as string
+            const base64 = reader.result as string;
             resolve(base64.replace(/^data:.+;base64,/, ''));
           };
-          reader.readAsDataURL(response);
+          reader.readAsDataURL(myBlob);
         });
-        data.then(res =>{
+        data.then(res => {
           this.src = res as string;
-        })
-      })
-  }
-  /**
+          this.isHiddenSpinner = true;
+        });
+      }, (error) =>{
+        this.isHiddenSpinner = true;
+      });
+    }
+
+  /*
    * Create file name
    */
   getRandomStringFileName() {
@@ -193,7 +202,7 @@ export class PdfViewerCustomerComponent implements OnInit {
     for ( let  i = 0; i < length; i++ ) {
         result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
     }
-    return result+".pdf";
+    return result + ".pdf";
   }
 
   /**
@@ -203,57 +212,52 @@ export class PdfViewerCustomerComponent implements OnInit {
     this.showToolbar = !this.showToolbar;
   }
 
+  async printPDF() {
+    this.isHiddenSpinner = false;
+    const iframe = document.createElement('iframe');
+    fetch(this.sourcePdf).then( response => {
+        return response.blob();
+    }).then( (myBlob) => {
+        iframe.src = window.URL.createObjectURL(new Blob([myBlob], {type: "application/pdf"}));
+        document.body.appendChild(iframe);
+        window.URL.revokeObjectURL(iframe.src);
+        this.isHiddenSpinner = true;
+    }).then(
+         () => {
+            setTimeout( () => {
+              iframe.contentWindow.print();
+              this.isHiddenSpinner = true;
+            }, 100);
+        },(error) => {
+            this.isHiddenSpinner = true;
+        });
+  }
+
+  downloadPDF(){
+    const element = document.getElementById("download");
+        element.dispatchEvent(new Event("click"));
+    }
+
   /**
    *
    */
   onZoomIn(){
-    if (this._oldZoomSetting===0){
-      this.zoomSetting = "110";
-    }
-    else{
-      const zoomAfter = this.zoomLevels.indexOf(Number(this._oldZoomSetting)*1.0/100);
-      if (zoomAfter !==-1 &&  Number(this._oldZoomSetting)*1.0/100 !== 4.0){
-          this.zoomSetting = String( Number(this.zoomLevels[zoomAfter+1])*100 );
-          this.toolTip = false;
-      }
-      else{
-        if ( Number(this._oldZoomSetting)*1.0/100 < 4.0){
-          this.zoomSetting = String( Number(this.zoomSetting) + 10 );
-          this.toolTip = false;
-        }
-        else{
-          this.toolTip = true;
-        }
-      }
-    }
+      const element = document.getElementById("zoomIn");
+      element.dispatchEvent(new Event("click"));
   }
 
   onZoomOut(){
-    if (this._oldZoomSetting===0){
-      this.zoomSetting = "90";
-    }
-    else{
-      const zoomAfter = this.zoomLevels.indexOf(Number(this._oldZoomSetting)*1.0/100);
-      if (zoomAfter !==-1 &&  Number(this._oldZoomSetting)*1.0/100 !== 0.1){
-          this.zoomSetting = String( Number(this.zoomLevels[zoomAfter-1])*100 );
-          this.toolTip = false;
-      }
-      else{
-        if ( Number(this._oldZoomSetting)*1.0/100 > 0.1){
-          this.zoomSetting = String( Number(this.zoomSetting) - 10 );
-          this.toolTip = false;
-        }
-        else{
-          this.toolTip = true;
-        }
-      }
-    }
+      const element = document.getElementById("zoomOut");
+      element.dispatchEvent(new Event("click"));
   }
 
-  currentZoomFactor(event){
-    this._oldZoomSetting = parseInt(event)
-  }
-
+  // currentZoomFactor(event){
+  //   const checkZoom = ['auto', 'page-actual', 'page-fit', 'page-width'];
+  //   if (checkZoom.includes(event)){
+  //     event = "-1";
+  //   }
+  //   this._oldZoomSetting = Number(event);
+  // }
 
   @HostListener('document:fullscreenchange', ['$event'])
   @HostListener('document:webkitfullscreenchange', ['$event'])
@@ -261,67 +265,66 @@ export class PdfViewerCustomerComponent implements OnInit {
   @HostListener('document:MSFullscreenChange', ['$event'])
   fullScreenMode() {
     if (document.fullscreenElement) {
+      this.sizePlatForm = '100vh';
+      this.backGroundColorPdf = "black";
+      this.flagFullScreen = true;
       this.eventFullScreen = false;
       this.hozFullScreen = true;
-      this.backGroundColorPdf = "black";
 
+      this.cdr.detectChanges();
     } else {
-      // const outlet = document.getElementById("outerContainer");
-      // outlet.parentElement.parentElement.parentElement.style.height = "80vh";
+      this.flagFullScreen = false;
+      this.sizePlatForm = this.sizeOldPlatForm;
       this.hozFullScreen = false;
       this.eventFullScreen = true;
-      this.sizePlatForm = this.sizeOldPlatForm;
       this.backGroundColorPdf = "white";
+      this.cdr.detectChanges();
+    }
+
+  }
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if (event.key === 'Escape' && this.flagFullScreen) {
+      this.sizePlatForm = this.sizeOldPlatForm;
+      this.flagFullScreen = false;
+      this.cdr.detectChanges();
     }
   }
 
   fullScreen(){
     const elem = document.getElementById("pdfElement")  as HTMLElement & {
-      requestFullscreen():Promise<void>;
+      requestFullscreen(): Promise<void>;
       mozRequestFullScreen(): Promise<void>;
       webkitRequestFullscreen(): Promise<void>;
       msRequestFullscreen(): Promise<void>;
     };
-    const buttonElement = document.createElement("mat-icon");
-    buttonElement.style.bottom = "0";
-    buttonElement.style.fontSize = "25px"
-    buttonElement.style.width = "40px"
-    buttonElement.style.height = "40px"
-    buttonElement.style.background = "#999"
-    buttonElement.style.borderRadius = "50%";
-    buttonElement.style.cursor = "pointer";
-    buttonElement.innerHTML = "fullscreen"
-    buttonElement.innerText = "fullscreen"
-    buttonElement.style.color = "#fff";
-    elem.appendChild(buttonElement);
-
-
-
-
-    // const outlet = document.getElementById("outerContainer")
-    // outlet.parentElement.parentElement.parentElement.style.height = "100vh";
+    this.flagFullScreen = true;
     this.sizePlatForm = '100vh';
     if (elem.requestFullscreen) {
+
       elem.requestFullscreen();
     } else if (elem.mozRequestFullScreen) {
       /* Firefox */
       elem.mozRequestFullScreen();
     } else if (elem.webkitRequestFullscreen) {
+
       /* Chrome, Safari and Opera */
       elem.webkitRequestFullscreen();
     } else if (elem.msRequestFullscreen) {
       /* IE/Edge */
       elem.msRequestFullscreen();
     }
+    this.cdr.detectChanges();
   }
 
   onHorfullScreen(){
-    const element = document.getElementById("presentationMode")
+    const element = document.getElementById("presentationMode");
     element.dispatchEvent(new Event("click"));
   }
-
   closeFullscreen() {
     const document: any = window.document;
+    this.flagFullScreen = false;
+    this.sizePlatForm = this.sizeOldPlatForm;
     if (document.exitFullscreen) {
       document.exitFullscreen();
     } else if (document.webkitExitFullscreen) {
@@ -331,14 +334,14 @@ export class PdfViewerCustomerComponent implements OnInit {
     } else if (document.msExitFullscreen) {
       document.msExitFullscreen();
     }
+    this.cdr.detectChanges();
   }
 
-  onReadedComple() {
+  onReadComplete() {
     if (this.pdfPage === this.howMuchPage) {
       this.readComplete.emit(true);
     } else {
       this.readComplete.emit(false);
     }
   }
-
 }
